099effd8231c822af38ce9e954180319ef2299ca
juslins <julio.lins@microsoft.com>
2015-09-14 22:03:22 +0100

    left over

--- questions/Graph/SameContact.cs
+++ questions/Graph/SameContact.cs
@@ -22,7 +22,7 @@ namespace ProgrammingQuestions
 
             foreach (var item in result)
             {
-                item.PrintToConsole();
+                item.PrintToConsole(); //my extension method
             }
         }
 
@@ -38,28 +38,38 @@ namespace ProgrammingQuestions
                 if (visited[i]) continue;
 
                 //if i is not visited, create a list and add all connected
-                var list = new List<int>();
-                var queue = new Queue<int>();
-                queue.Enqueue(i);
-
-                while (queue.Count > 0)
-                {
-                    int v = queue.Dequeue();
-                    visited[v] = true;
-                    list.Add(v);
-                    foreach (var node in graph[v])
-                    {
-                        if (!visited[node])
-                        {
-                            queue.Enqueue(node);
-                        }
-                    }
-                }
+                var list = ListConnected(i, graph, visited);
 
                 result.Add(list);
             }
 
             return result;
+        }
+
+        //non-recursive Bfs
+        //adds each non-visited node to the list
+        private static List<int> ListConnected(int initialNode, Dictionary<int, List<int>> graph, bool[] visited)
+        {
+            var list = new List<int>();
+            var queue = new Queue<int>();
+            queue.Enqueue(initialNode);
+
+            while (queue.Count > 0)
+            {
+                int v = queue.Dequeue();
+                visited[v] = true;
+                list.Add(v);
+
+                foreach (var node in graph[v])
+                {
+                    if (!visited[node])
+                    {
+                        queue.Enqueue(node);
+                    }
+                }
+            }
+
+            return list;
         }
 
         public static Dictionary<int, List<int>> BuildGraph(string[][] contacts)
@@ -68,10 +78,13 @@ namespace ProgrammingQuestions
             var graph = new Dictionary<int, List<int>>();
             var map = new Dictionary<string, List<int>>();
 
+            //iterate through each contact
             for (int i = 0; i < contacts.Length; i++)
             {
+                //add contact to graph
                 graph.Add(i, new List<int>());
 
+                //iterate through each field
                 for (int j = 0; j < contacts[i].Length; j++)
                 {
                     string key = contacts[i][j];
@@ -81,7 +94,7 @@ namespace ProgrammingQuestions
                         //if this is a repeat, then add connections to the graph
                         Connect(graph, map[key], i);
 
-                        //then add it to the existing set for key
+                        //then add it to the existing set for the key
                         map[key].Add(i);
                     }
                     else
@@ -96,8 +109,10 @@ namespace ProgrammingQuestions
 
         private static void Connect(Dictionary<int, List<int>> graph, IEnumerable<int> nodes, int newNode)
         {
-            //bidirectional graph
+            //bidirectional graph: add existing nodes to newNode
             graph[newNode].AddRange(nodes);
+
+            //add newNode to each existing node
             foreach (var node in nodes)
             {
                 graph[node].Add(newNode);
