e2b8022234cfe10c2ef485ba6a4c5981c36811bb
Julio Lins <juslins@microsoft.com>
2015-10-27 18:53:30 +0000

    merge fix

--- questions/Sorting/Sortings.cs
+++ questions/Sorting/Sortings.cs
@@ -18,12 +18,51 @@ namespace ProgrammingQuestions.Sorting
             MergeSort(a);
             a.PrintToConsole();
             a.AssertSorted();
+
+            Console.WriteLine("Number of inversions: " + numberOfInversions);
+        }
+
+        private static int numberOfInversions = 0;
+
+        public static void BottonUpMergeSort(int[] array)
+        {
+            int[] aux = new int[array.Length];
+            int step = 1;
+
+            while (step < array.Length)
+            {
+                for (int i = 0; i < array.Length - step; i += step)
+                {
+                    Merge(array, i, i + (step / 2), Math.Max(i + step, array.Length - 1), aux);
+                }
+
+                step *= 2;
+            }
         }
 
         public static void MergeSort(int[] array)
         {
             DoMergeSort(array, 0, array.Length - 1, new int[array.Length]);
         }
+
+        public static void HalfAuxMergeSort(int[] array)
+        {
+            DoHalfSizeMergeSort(array, 0, array.Length - 1, new int[array.Length / 2 + 1]);
+        }
+
+        private static void DoHalfSizeMergeSort(int[] array, int start, int end, int[] aux)
+        {
+            if (start == end)
+            {
+                return;
+            }
+
+            int middle = (start + end) / 2;
+            DoHalfSizeMergeSort(array, start, middle, aux);
+            DoHalfSizeMergeSort(array, middle + 1, end, aux);
+            MergeHalfArray(array, start, middle, end, aux);
+        }
+
 
         private static void DoMergeSort(int[] array, int start, int end, int[] aux) 
         {
@@ -52,10 +91,30 @@ namespace ProgrammingQuestions.Sorting
             {
                 if (i > middle) array[k] = aux[j++];
                 else if (j > end) array[k] = aux[i++];
-                else if (array[i] > array[j]) array[k] = array[j++];
-                else array[k] = array[i++];
+                else if (aux[i] > aux[j]) { array[k] = aux[j++]; numberOfInversions++; }
+                else array[k] = aux[i++];
             }
         }
+
+        public static void MergeHalfArray(int[] array, int start, int middle, int end, int[] aux)
+        {
+            for (int p = 0; p <= (middle - start); p++)
+            {
+                aux[p] = array[start + p];
+            }
+
+            int i = 0;
+            int j = middle + 1;
+
+            for (int k = start; k <= end; k++)
+            {
+                if ((i + start) > middle) array[k] = array[j++];
+                else if (j > end) array[k] = aux[i++];
+                else if (aux[i] > array[j]) array[k] = array[j++];
+                else array[k] = aux[i++];
+            }
+        }
+
 
 
         public static void InsertionSort(int[] array)
