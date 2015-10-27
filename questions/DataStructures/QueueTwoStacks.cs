692871336c6fc03d5643de3f6757b24a89376377
Julio Lins <juslins@microsoft.com>
2015-10-27 14:39:09 +0000

    data

---
+++ questions/DataStructures/QueueTwoStacks.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingQuestions.DataStructures
+{
+    public class QueueTwoStacks
+    {
+        private Stack usedStack, emptyStack;
+
+        public QueueTwoStacks(int size)
+        {
+            usedStack = new Stack(size);
+            emptyStack = new Stack(size);
+        }
+
+        public void Enqueue(int x)
+        {
+
+
+
+        }
+    }
+}
---
+++ questions/DataStructures/Stack.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingQuestions.DataStructures
+{
+    public class Stack
+    {
+        private int[] elements;
+        private int index = -1;
+
+        public Stack(int size)
+        {
+            elements = new int[size];
+        }
+
+        public void Push(int x)
+        {
+            index++;
+
+            if (index < elements.Length)
+            {
+                elements[index] = x;
+            }
+            else
+            {
+                throw new Exception("Stack is full");
+            }
+        }
+
+        public int Pop()
+        {
+            if (index < 0)
+            {
+                throw new Exception("Stack is empty");
+            }
+
+            return elements[index--];
+        }
+
+        public bool IsEmpty()
+        {
+            return index < 0;
+        }
+    }
+}
