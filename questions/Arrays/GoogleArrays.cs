c74fd1905667a7a85b5f153d661e5230b816d312
Julio Lins <juslins@microsoft.com>
2015-12-18 15:59:07 +0000

    jumps

--- questions/Arrays/GoogleArrays.cs
+++ questions/Arrays/GoogleArrays.cs
@@ -8,6 +8,12 @@ namespace ProgrammingQuestions.Arrays
 {
     public class GoogleArrays
     {
+        public static void Test()
+        {
+            int[] array = new int[] { 1, 3, 5, 8, 9, 2, 6, 7, 6, 8, 9 };
+            Console.WriteLine(MinimumJumps(array));
+        }
+
 
         //http://www.geeksforgeeks.org/minimum-number-of-swaps-required-for-arranging-pairs-adjacent-to-each-other
         /*
@@ -29,6 +35,43 @@ namespace ProgrammingQuestions.Arrays
             }
 
             return swaps;
+        }
+
+        //http://www.geeksforgeeks.org/minimum-number-of-jumps-to-reach-end-of-a-given-array/
+        //Minimum number of jumps to reach end
+        //Input: arr[] = {1, 3, 5, 8, 9, 2, 6, 7, 6, 8, 9}
+        //Output: 3 (1-> 3 -> 8 ->9)
+        public static int MinimumJumps(int[] array)
+        {
+            return Jump(array, 0, new int[array.Length]);
+        }
+
+        private static int Jump(int[] array, int position, int[] cache)
+        {
+            if (position >= array.Length - 1)
+            {
+                return 0;
+            }
+
+            if (cache[position] > 0)
+            {
+                return cache[position];
+            }
+
+            int jumps = int.MaxValue;
+
+            for (int i = 1; i <= array[position]; i++)
+            {
+                int pathJumps = Jump(array, position + i, cache);
+
+                if (pathJumps < jumps)
+                {
+                    jumps = pathJumps;
+                }
+            }
+
+            cache[position] = jumps + 1;
+            return cache[position];
         }
     }
 }
